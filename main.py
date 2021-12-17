0812c88ed6766b7441cfee399ecfe6c13750e8a8
= <=>
2021-12-17 15:10:35 +0100

    Entitées Alien/Joueur avec affichage et mouvement de base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import copy
 
 HEIGHT = 920
 WIDTH = 1280
@@ -16,7 +17,7 @@ class canvaSP():
         self.canv = tk.Canvas(self.mw, height = self.height, width = self.width)
         self.canv.pack()
 
-        self.player = None
+        self.player = Vaisseau(self.canv, 500,500,10,10, vx0 = 0.1, vy0 = 0.1)
         self.aliens = []
         self.murs = []
         self.projectiles = []
@@ -27,10 +28,16 @@ class canvaSP():
         self.Collision()
 
     def loadLevel(self):
-        # open...
+    # TO DO : open...
+        (x0,y0,x1,y1) = self.canv.coords(self.player.obj)
+        vx,vy = self.player.vx, self.player.vy
         self.canv.delete('all')
-        level = [Vaisseau(500, 500 , 25, 0, 0,self.canv), [Vaisseau(0, 0 , 25, 0.1, 0,self.canv)], [], []]
-        [self.player, self.aliens, self.murs, self.projectiles] = level
+        self.player = Vaisseau(self.canv, x0, y0, x1-x0 ,y1-y0 , vx0 = vx, vy0 = vy)
+
+    # TO DO : Place player at base of screen
+
+        level = [ [Vaisseau(self.canv, 0, 0 , 25, 25,  vx0 = 0.1, vy0 = 0)], [], []]
+        [ self.aliens, self.murs, self.projectiles] = level
 
 
     def Collision(self): 
@@ -41,29 +48,47 @@ class canvaSP():
         
 
         self.windowCollisions()
+        self.manageCollision(listeCollision)
         self.updatePositions()
         self.mw.after(PERIOD, self.Collision)
 
     def windowCollisions(self):
         # Manage collisions between aliens and the screen's edges
         for alien in self.aliens:
-            print(alien.coord())
-            (x0,y0,x1,y1) = alien.coord()
-            if x0 <= 0 + alien.size:
+            (x0,y0,x1,y1) = self.canv.coords(alien.obj)
+            # Collision left of screen
+            if x0 <= 0:
                 alien.vx = abs(alien.vx)
-            elif x0 >= WIDTH - alien.size:
+            # Collision right of screen
+            elif x1 >= WIDTH:
                 alien.vx = -abs(alien.vx)
+            # Collision top of screen
+            elif y0 <= 0:
+                alien.vy = abs(alien.vy)
+            # Collision bottom of screen
+            elif y1 >= HEIGHT:
+                alien.vy = -abs(alien.vy)
 
         # Manage collisions between the player and the screen's borders
-        (x0,y0,x1,y1) = self.player.coord()
-        if x0 <= 0 + self.player.size :
-            self.player.vx = abs(self.player.vx)
-        elif x0 >= WIDTH - self.player.size:
-            self.player.vx = -abs(self.player.vx)
+        (x0,y0,x1,y1) = self.canv.coords(self.player.obj)
+        # Limit player movement to the screen
+
+        # Limit left movement to the screen
+        if x0 < 0 :
+            self.player.Move(abs(x0),0)
+        # Limit right movement to the screen
+        if x1 > WIDTH :
+            self.player.Move(WIDTH - x1, 0)
+        # Limit top movement to the screen
+        if y0 < 0:
+            self.player.Move(0,abs(y0))
+        # Limit bottom movement to the screen
+        if y1 > HEIGHT:
+            self.player.Move(0, HEIGHT - y1)
 
         # Manage collisions between the projectiles and the screen's borders
 
-    def manageCollision(self, obj, collision):
+    def manageCollision(self, listeCollision):
         return 0
 
 
@@ -76,20 +101,22 @@ class canvaSP():
 
 
 class Vaisseau():
-    def __init__(self,x0,y0,size, vx0, vy0,canvas):
+    def __init__(self, canvas,  x0, y0, sizex, sizey, vx0=0, vy0=0 ):
         self.vx = vx0
         self.vy = vy0
 
         self.canvas = canvas
-        self.obj  = canvas.create_rectangle(x0 , y0 , x0 + size , y0 + size,  fill = "maroon")
-
-    def size(self):
-        return (
+        self.obj  = canvas.create_rectangle(x0 , y0 , x0 + sizex , y0 + sizey,  fill = "maroon")
 
     def updatePositionOnCanvas(self):
-        #self.x += self.vx * PERIOD
-        self.canvas.move(self.obj, self.vx *PERIOD, self.vy*PERIOD)
-        #self.canvas.coords(self.obj, self.x , self.y , self.x +self.size, self.y +self.size)
+        self.Move( self.vx * PERIOD, self.vy * PERIOD)
+
+
+    def Move(self,x,y):
+        self.canvas.move(self.obj, x, y)
+    
+    def ResetPosition(self, x, y):
+        self.canvas.coords(self.obj, x, y)
 
 
     
