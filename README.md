3d2bb1eebad339ca86b231358efaf669df3dc517
PerolJ <julien.perol@cpe.fr>
2022-01-04 10:57:47 +0100

    collisions de base opérationnelles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CS-DEV-Project-1
 CS-DEV Semester 1; Project Space Invader
+
+
+Fonctionnalités actuelles : Présence d'alien et du player. Tir possible. Collisions ok.
+To do : Level /niveau / 
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import tkinter as tk
 import copy
 from math import pi, cos, sin, atan
 
+
+from collision import CollisionCheck
+
 FPS = 60
 
-HEIGHT = 800
-WIDTH = 1200
+HEIGHT = 600
+WIDTH = 1000
 
 XMIN = 0
 XMAX = WIDTH
@@ -95,7 +98,8 @@ class canvaSP():
             if touche == "space":
                 self.touches.append(touche)
                 self.player.AutoShoot()
-            if touche in ['Left', 'Right', 'Up', 'Down']:
+            if touche in ['Left', 'Right', 'Up', 'Down']: 
+            #if touche in ['Left', 'Right']:
                 self.touches.append(touche)
                 self.SetPlayerSpeed()
 
@@ -112,14 +116,18 @@ class canvaSP():
     def loadLevel(self):
     # TO DO : open...
         # Save the current player coordinates
-        (x0,y0,x1,y1) = self.canv.coords(self.player.obj)
-        # vx,vy = self.player.vx, self.player.vy
+        if self.player != None:
+            (x0,y0,x1,y1) = self.canv.coords(self.player.obj)
+            # vx,vy = self.player.vx, self.player.vy
 
-        self.canv.delete('all')
+            self.canv.delete('all')
 
-        x = (XMIN + XMAX) / 2
-        y = (YMAX - (y1-y0))
-        self.player = Vaisseau(self.canv, x, y, x1-x0 ,y1-y0)
+            x = (XMIN + XMAX) / 2
+            y = (YMAX - (y1-y0))
+            self.player = Vaisseau(self.canv, x, y, x1-x0 ,y1-y0)
+        else:
+            self.canv.delete('all')
+            self.player = Vaisseau(self.canv, 500,500,10,10, vx0 = 0, vy0 = 0)
 
         level = [ [Vaisseau(self.canv, 0, 0 , 25, 25,  vx0 = 0.1, vy0 = 0)], [], []]
         [ self.aliens, self.murs, self.projectiles] = level
@@ -129,6 +137,58 @@ class canvaSP():
         # [[coll player/aliens], [coll player/aliens] ,
         # [coll murs/projectiles], [aliens/murs], [aliens/projectiles] ]
         listeCollision = [[] for i in range(5)]
+
+        # For each individual projectile, checks every potential collision with player, aliens, walls
+        for i, projectile in enumerate(self.projectiles):
+            
+            # With player
+            if CollisionCheck(self, projectile, self.player):
+                self.canv.delete(self.projectiles[i].obj)
+                del self.projectiles[i]
+                self.canv.delete(self.player.obj)
+                del self.player
+                self.loadLevel()
+            
+            # With aliens
+            for j, alien in enumerate(self.aliens):
+                if CollisionCheck(self, alien,projectile):
+                    self.canv.delete(self.projectiles[i].obj)
+                    del self.projectiles[i]
+                    self.canv.delete(self.aliens[j].obj)
+                    del self.aliens[j]
+
+
+            # With walls from top and bottom of the wall
+            #for j, wall in enumerate(self.walls):
+                #if CollisionCheck(wall, projectile):
+                    #self.canv.delete(self.projectiles[i].obj)
+                    #del self.projectiles[i]
+                    #self.canv.delete(self.walls[j].obj)
+                    #del self.walls[j]
+
+
+
+
+        # For each individual wall, checks every potential collision with player, aliens
+        # No need for player/wall since player should be locked beyond walls
+        #for i, wall in enumerate(self.walls):
+             # With aliens
+            #for j, alien in enumerate(self.aliens):
+                #if CollisionCheck(wall, alien):
+                    #self.canv.delete(self.walls[i].obj)
+                    #del self.walls[i]
+                    
+
+
+        # For each individual alien, checks every potential collision with player
+        for i, alien in enumerate(self.aliens):
+            if CollisionCheck(self, alien, self.player):
+                self.canv.delete(self.player.obj)
+                self.player = None
+                self.loadLevel()
+        
+
+        
 
         
         # Check collisions with the screen limits
